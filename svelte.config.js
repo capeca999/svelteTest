c46cb6b838242e276e64d1de689799fe272750c0
capeca999 <47533906+capeca999@users.noreply.github.com>
2025-08-03 20:49:33 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
